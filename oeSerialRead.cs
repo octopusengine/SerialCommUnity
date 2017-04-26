396e49b7f3b9febccf9e0dc250852226ae92be84
Honza S. Copak <bonum.comunae@volny.cz>
2017-04-26 10:23:57 +0200

    Add files via upload

---
+++ oeSerialRead.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Sample for reading using polling by yourself. In case you are fond of that.
+
+public class oeSerialRead : MonoBehaviour
+{
+    public int indexData = 7;
+    public bool debugLog = false;
+
+    public SerialController serialController;
+
+
+
+
+    // Initialization
+    void Start()
+    {
+        serialController = GameObject.Find("SerialController").GetComponent<SerialController>();
+    }
+
+    // Executed each frame
+    void Update()
+    {
+        string message = serialController.ReadSerialMessage();
+
+        if (message == null)
+            return;
+
+        // Check if the message is plain data or a connect/disconnect event.
+        if (ReferenceEquals(message, SerialController.SERIAL_DEVICE_CONNECTED))
+            Debug.Log("Connection established");
+        else if (ReferenceEquals(message, SerialController.SERIAL_DEVICE_DISCONNECTED))
+            Debug.Log("Connection attempt failed or disconnection detected");
+        else
+            if (debugLog) Debug.Log("Message arrived: " + message);
+           oeCommonDataContainer.setArrInt(indexData, System.Int32.Parse(message));
+    }
+}
